79954e1b26f99a1c07152fd2eb84a3164e2b96c8
Andrew <acekane1@gmail.com>
2018-05-14 19:16:14 -0700

    Change formatter conservatively

--- lib/logstop/railtie.rb
+++ lib/logstop/railtie.rb
@@ -2,8 +2,10 @@ module Logstop
   class Railtie < Rails::Railtie
     console do
       # make broadcaster inherit formatter
-      # yes, these are the same, but it works
-      Rails.logger.formatter = Rails.logger.formatter
+      if Rails.logger && Rails.logger.formatter.is_a?(Logstop::Formatter)
+        # yes, these are the same, but it works
+        Rails.logger.formatter = Rails.logger.formatter
+      end
     end
   end
 end
