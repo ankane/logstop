8c6e9956ce5287b541823d6884981a09fa7fa095
Andrew Kane <andrew@chartkick.com>
2019-02-24 15:56:25 -0800

    Better naming [skip ci]

--- lib/logstop.rb
+++ lib/logstop.rb
@@ -6,8 +6,8 @@ module Logstop
   FILTERED_STR = "[FILTERED]".freeze
   FILTERED_URL_STR = "\\1[FILTERED]@".freeze
 
-  CREDIT_CARD_REGEX = /\b\d{4}[\s+-]\d{4}[\s+-]\d{4}[\s+-]\d{4}\b/
-  CREDIT_CARD_REGEX2 = /\b\d{16}\b/
+  CREDIT_CARD_REGEX = /\b\d{16}\b/
+  CREDIT_CARD_REGEX_DELIMITERS = /\b\d{4}[\s+-]\d{4}[\s+-]\d{4}[\s+-]\d{4}\b/
   EMAIL_REGEX = /\b[\w][\w+.-]+(@|%40)[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\b/i
   IP_REGEX = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
   PHONE_REGEX = /\b(\+\d{1,2}\s)?\(?\d{3}\)?[\s+.-]\d{3}[\s+.-]\d{4}\b/
@@ -22,7 +22,7 @@ module Logstop
       .gsub(URL_PASSWORD_REGEX, FILTERED_URL_STR)
       .gsub(EMAIL_REGEX, FILTERED_STR)
       .gsub(CREDIT_CARD_REGEX, FILTERED_STR)
-      .gsub(CREDIT_CARD_REGEX2, FILTERED_STR)
+      .gsub(CREDIT_CARD_REGEX_DELIMITERS, FILTERED_STR)
       .gsub(PHONE_REGEX, FILTERED_STR)
       .gsub(SSN_REGEX, FILTERED_STR)
 
